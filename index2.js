dfa841a0e98b06315776fe1d657663c7e60f857d
Kiup Kim <kukim96@gmail.com>
2021-04-08 17:33:03 -0600

    promised with answer from compass

---
+++ index2.js
@@ -0,0 +1,13 @@
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised');
+
+fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(body => console.log(body));
+
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  });
